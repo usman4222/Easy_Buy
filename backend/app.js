2f358184a0bb095e2b94e5ab3149f4dc99d12cbd
usman4222 <usman4243ch@gmail.com>
2024-12-08 04:15:37 -0800

    update origin

--- backend/app.js
+++ backend/app.js
@@ -18,20 +18,14 @@ dotenv.config();
 
 // Middleware
 
-const allowedOrigins = [
-  "http://localhost:5173", 
-  "https://easy-buy-hm3t.vercel.app/"
+// const allowedOrigins = [
+//   "http://localhost:5173", 
+//   "https://easy-buy-hm3t.vercel.app/"
 
-];
+// ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); 
-    } else {
-        callback(new Error("Not allowed by CORS")); 
-    }
-},
+  origin: "http://localhost:5173",
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],

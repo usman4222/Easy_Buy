0822f435adbb52f8c99855f14800f1c3ba76d67d
usman4222 <usman4243ch@gmail.com>
2025-01-09 15:47:38 -0500

    remove id from update user

--- frontend/src/actions/UserAction.js
+++ frontend/src/actions/UserAction.js
@@ -32,8 +32,8 @@ import {
   LOAD_SUCCESS,
 } from "../redux/userSlice/myProfileSlice";
 
-let apiurl = "https://easy-buy-s9rh.vercel.app"
-// let apiurl = "http://localhost:4000";
+// let apiurl = "https://easy-buy-s9rh.vercel.app"
+let apiurl = "http://localhost:4000";
 
 export const login = (userData) => async (dispatch) => {
   try {
@@ -54,6 +54,8 @@ export const login = (userData) => async (dispatch) => {
 
     dispatch(LOGIN_SUCCESS(data.user));
   } catch (error) {
+    console.log("error",error);
+    
     dispatch(LOGIN_FAIL(error.response.data.message));
   }
 };
